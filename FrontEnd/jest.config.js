2f0ccfdd6ba2ab459d065d8a1a382ff77a6d9d31
alfredGutiHaz <dibandaalfred@gmail.com>
2022-05-13 17:45:28 +0200

    debut muti module project

--- FrontEnd/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-    setupFiles: ['./tests/unit/setup.ts'],
-    testMatch: ['*/src//?(.)+(spec|test).[jt]s']
-}
--- GestionDepense/src/main/java/com/usage/domain/Note.java
+++
@@ -1,9 +0,0 @@
-package com.usage.domain;
-
-import java.util.List;
-
-public class Note {
-
-    private List<String> values;
-
-}
